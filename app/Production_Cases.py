6a8f704461a140e678be0f6fa54f617509670532
marioautomacao5 <marioautomacao5@gmail.com>
2026-03-23 20:56:08 -0300

    Upload mapped database

--- app/Production_Cases.py
+++ app/Production_Cases.py
@@ -40,7 +40,7 @@ st_autorefresh(interval=60000)
 # ======================================================
 
 st.title("🏭 Packging view dashboard")
-st.caption("Refresh up to one minute")
+st.caption("Refresh each minute")
 st.divider()
 
 # ======================================================
